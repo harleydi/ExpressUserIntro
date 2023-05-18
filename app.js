322076c46159988bcc123d91938b6af51e4a4cdc
harleydi <disneyharley26@gmail.com>
2023-05-18 19:03:04 -0400

    added all-users route

--- app.js
+++ app.js
@@ -10,6 +10,10 @@ app.get("/", (req, res) => {
     res.send("Hello World")
 })
 
+app.get("/all-users", (req, res) => {
+    return res.status(200).json({ data: userList })
+})
+
 
 app.listen(PORT, () => {
     console.log(`Example app listening on port ${PORT}`);
