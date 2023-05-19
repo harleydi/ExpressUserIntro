3691488406e2dd1ff4e5c755320cb2514b3180ea
harleydi <disneyharley26@gmail.com>
2023-05-18 22:03:04 -0400

    added getUserByNumber

--- app.js
+++ app.js
@@ -10,8 +10,20 @@ app.get("/", (req, res) => {
     res.send("Hello World")
 })
 
+// GET all users
 app.get("/all-users", (req, res) => {
     return res.status(200).json({ data: userList })
+})
+
+// GET single-user
+app.get("/single-user/:phoneNumber", (req, res) => {
+    const number = req.params.phoneNumber
+    const getUserByNumber = userList.filter((user) => {
+        if (user.phone === number) {
+            return user
+        }
+    })
+    res.status(200).json({ data: getUserByNumber })
 })
 
 
